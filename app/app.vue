6d114298b6316f1ba7d48cdc7e006d2917272d65
Ivan Ugnenko <vanchopi@yahoo.com>
2026-02-17 18:04:14 +0400

    deploy test

--- app/app.vue
+++ app/app.vue
@@ -10,3 +10,10 @@
 <script setup lang="ts">
 const route = useRoute();
 </script>
+<style lang="scss">
+body .v-application{
+  background: #3B4752;
+  background: radial-gradient(circle,rgba(59, 71, 82, 1) 0%, rgba(39, 195, 184, 1) 46%);
+  color: #A9B4C0;
+}
+</style>
